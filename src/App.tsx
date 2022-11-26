39793e2e2d9c61576dcce9977f72f23b9c399e90
jk1955 <jim@wecancodeit.org>
2022-11-26 12:22:10 -0500

    I tried incorporating Kolton's AI functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,62 @@ import React, { useState } from "react";
 import "./App.css";
 import Chessboard from "chessboardjsx";
 import { ChessInstance, ShortMove } from "chess.js";
+import EvaluateBoard from "./ChessEngine/EvaluateBoard";
+import GetBestMove from "./ChessEngine/GetBestMove";
 
 const Chess = require("chess.js");
 
+let globalSum = 0;
+
 const App: React.FC = () => {
+  const [game, setGame] = useState(new Chess());
+
+// Kolton's Work
+//====================================================================
+ 
+// perform modify function on game state
+function safeGameMutate(game: any) {
+  setGame((g: any) => {
+    const update = { ...g };
+    setGame(update);
+    return update;
+  });
+}
+
+function makeBestMove(color: string) {
+  var move: string | ShortMove = "";
+  if (color === "b") {
+    move = GetBestMove(game, color, globalSum)[0];
+  } else {
+    move = GetBestMove(game, color, -globalSum)[0];
+  }
+  globalSum = EvaluateBoard(move, globalSum, "b");
+
+  console.log(globalSum);
+  game.move(move);
+
+  return move;
+}
+
+// make computer move
+function AIMove() {
+  var move: string | ShortMove = "";
+
+  const possibleMove: string | ShortMove = makeBestMove("b");
+  // exit if the game is over
+  if (game.game_over() || game.in_draw()) return;
+
+  safeGameMutate((game: { move: (arg0: string | ShortMove) => any; }) => {
+    move = game.move(possibleMove);
+    return move;
+    // play();
+  });
+
+  return move!;
+}
+
+//====================================================================
+
   const [chess] = useState<ChessInstance>(
     new Chess("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
   );
@@ -21,6 +73,10 @@ const App: React.FC = () => {
         if (moves.length > 0) {
           const computerMove = moves[Math.floor(Math.random() * moves.length)];
           chess.move(computerMove);
+
+          // chess.move(AIMove()!);
+          AIMove();
+          
           setFen(chess.fen());
         }
       }, 300);
